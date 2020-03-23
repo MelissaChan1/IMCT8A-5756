633e57a6ffce5f7610cd607fb8561dff3508f74b
MelissaChan1 <meli.chancua@gmail.com>
2020-03-23 17:02:29 -0600

    ultimos

---
+++ nuevos/masa.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<math.h>
+
+using namespace std;
+
+int ObtVal(int m, int T)
+{
+int A,teta,w;
+        w=m*9.81;
+        teta=acos(w/T);
+        A=T*sin(teta);
+return A;
+}
+
+int main()
+{
+	int T,m,A; 
+cout<<"Ingrese la tensión en el cable"<<endl;
+	cin>>T;
+cout<<"Ingrese la masa"<<endl;
+	cin>>m;
+A=ObtVal(m,T);
+
+cout<<"La fuerza requerida en A es: "<<A<<" N"<<endl;
+
+return 0;
+}
