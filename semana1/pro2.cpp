a473c1dfb884b0ba7392a9ea0ef7f31fa745436a
MelissaChan1 <meli.chancua@gmail.com>
2020-06-08 13:51:02 -0500

    otro

--- semana1/pro2.cpp
+++ semana1/pro2.cpp
@@ -1,67 +1,41 @@
+#include <bits/stdc++.h> 
 #include<iostream>
-#include<cstdlib>
-#include<ctime>
-
 using namespace std;
 
-int fact(int n){
-int i, f=1;
-        if(n<0)
-                f=0;
-        else if(n==0)
-                f=1;
-        else
-{
-        for(i=1;i<=n;i++)
-        f=f*i;
-}
-return f;
-}
+int con=0;
+int n,x,i,k,j,l;
+int i,k,j,l;
+int e[100];
 
 int main()
 {
-srand(time(NULL));
-
-int n, sum, s=0, a, b, c, d, e;
-cin>>n;
-cin>>sum;
-int nums[n];
-        for(int i=0;i<n;i++)
-                cin>>nums[i];
-a=fact(n);
-b=fact(4);
-c=n-4;
-d=fact(c);
-e=a/(b*d);
-int size=4;
-int ind;
-
-        for(int i=0;i<=e;i++)
-
-{
-        for(int i=0;i<n;i++)
-        cout<<nums[i]<<" ";
-
-        while(size>0)
-{
-ind=rand()%size;
-s=s+nums[ind];
-int aux;
-        while(ind<size)
+	for ( i = 0; i <n - 3; i++) 
+{ 
+    for ( j = i + 1; j <=n - 2; j++) 
+    {           
+        for ( k = j + 1; k <=n - 1; k++) 
         {
-        nums[ind]=nums[ind+1];
-        nums[ind+1]=0;
-        ind++;
+            for ( l = k + 1; l < n; l++) 
+	if (e[i] + e[j] + e[k] + e[l] == x)
+            if(con ==0 ){
+                cout << i+1<<", " << j+1  
+                     << ", " << k+1 << ", " << l+1<<endl;
+                     con+=+1;
+			}
+		}
+	}  
+} 
+	int n=sizeof(e) / sizeof(e[0]);
+{
+	cin>>n>>x;
+	for(i=1;i<=n;i++)
+	{
+		cin>>e[i];
 }
-
-        size--;
-
-        for(int i=0;i<=n;i++)
-        cout<<nums[i]<<" ";
 }
-        cout<<"la suma es: "<<s<<endl;
+	if(con==0)
+	{
+		cout<<"\n imposible";
+	}
+	return 0;
 }
-cout<<endl;
-return 0;
-}
-
--- semana1/pro3.cpp
+++ semana1/pro3.cpp
@@ -1,27 +1,46 @@
-#include<iostream>
-
+#include <iostream>
+#include <string>
 using namespace std;
 
-int ingre(int a)
-{
-int i;	
-int nums[a];	
-	for(i=0;i<a;i++)
-		cin>>nums[i];
-
-return nums[i];
+long* obt_a(long n){
+long* a=new long[n];
+for(int i=0;i<n;i++)
+cin>>a[i];
+return a;
 }
 
-int main()
-{
-	int n, m, k;
-cin>>n;
-cin>>m;
-cin>>k;
-int a=ingre(n);
-cout<<a<<endl;
-	int soli[n]={ingre(n)};
-	for(int i=0;i<n;i++)
-	cout<<soli[n]<<" ";
+long* obt_b(int m){
+long* b=new long[m];
+for(int i=0;i<m;i++)
+cin>>b[i];
+return b;
+}
+
+int obtener_r(long* a,long* b,long n,int m,long k){
+int r=0;
+for(int i=0;i<m;i++){
+for(long j=0;j<n;j++){
+if((a[j]<=(b[i]+k))&&(a[j]>=(b[i]-k))){
+r+=1;b[i]=0;}
+}}
+
+return r;
+
+}
+
+int main(){
+
+int m;
+long n,k;
+cin>>n>>m>>k;
+long* a=new long[n];
+long* b=new long[m];
+if(n>=1&&m<=200000&&(0<=k<=1000000000)){
+a=obt_a(n);
+b=obt_b(m);
+int r=obtener_r(a,b,n,m,k);
+cout<<r<<endl;
+}
 return 0;
+
 }
